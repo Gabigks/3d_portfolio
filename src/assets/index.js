01a825359709d59da41494fdeebbffaaed2fa3f1
GabrielKolling <gabrielgks10@gmail.com>
2024-06-25 14:58:18 -0300

    ajustes finais

--- src/assets/index.js
+++ src/assets/index.js
@@ -9,24 +9,21 @@ import close from "./close.svg";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 import n8n from "./tech/n8n.svg";
 import manychat from "./tech/manychat.png";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import amo from "./company/amo.png";
+import trafegus from "./company/trafegus.png";
+import desbravador from "./company/desbravador.png";
+import brisa from "./company/brisa.png";
 
 import leads from "./leads.jpg";
 import n8nproject from "./n8n.png";
@@ -43,21 +40,18 @@ export {
 	close,
 	css,
 	docker,
-	figma,
 	git,
 	html,
 	javascript,
-	mongodb,
 	nodejs,
 	reactjs,
-	redux,
 	tailwind,
 	typescript,
 	threejs,
-	meta,
-	shopify,
-	starbucks,
-	tesla,
+	amo,
+	trafegus,
+	desbravador,
+	brisa,
 	leads,
 	docusign,
 	n8n,
